b928ab9c3c42fa580a9bd8fa1a5bd65fbbeb9a20
MxMElmBs <eagbezou@hotmail.com>
2023-10-25 15:46:27 +0000

    Intial Project

---
+++ src/tp0/TP0.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0;
+
+/**
+ *
+ * @author eagbe
+ */
+public class TP0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour pour le TPO");
+    }
+    
+}
